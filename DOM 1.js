826c117e112e56c4464abd916fd9c319a05f373b
hafidh khalifah <hafidhkhalifah@gmail.com>
2023-06-26 11:46:26 +0700

    update materi pendahuluan DOM

--- DOM 1.js
+++ DOM 1.js
@@ -1,3 +1,9 @@
 /* 
-
+    DOM (document object model)
+    ==> interface yang merepresentasikan elemen didalam documen html menjadi object sehingga dapat elemen html tersebut dapat diakses dan dimanipulasi menggunakan program.
+    materi yang akan di pelajari:
+    DOM Tree
+    DOM Manipulation
+    DOM Tranversal
+    Event Handling
 */
